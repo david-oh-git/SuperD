8425f537732902ba3d2edcf0faf3a0ca0b5ada3b
dave-oh-git <hirakoshinji722@gmail.com>
2019-10-29 14:07:07 +0100

    +++ implement download repository : DefaultRepository

--- app/src/main/java/io/audioshinigami/superd/data/repository/DefaultRepository.kt
+++ app/src/main/java/io/audioshinigami/superd/data/repository/DefaultRepository.kt
@@ -1,12 +1,17 @@
 package io.audioshinigami.superd.data.repository
 
+import android.net.Uri
 import com.tonyodev.fetch2.Fetch
+import com.tonyodev.fetch2.NetworkType
+import com.tonyodev.fetch2.Priority
+import com.tonyodev.fetch2.Request
 import io.audioshinigami.superd.data.source.db.dao.FileDataDao
 import io.audioshinigami.superd.data.source.db.entity.FileData
 import io.audioshinigami.superd.utility.ReUseMethods
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 
 /*implementation for database repository as data source */
 
@@ -17,9 +22,22 @@ class DefaultRepository(
     private val fetch: Fetch
 ) : DataBaseRepository, DownloadRepository {
 
-    private fun createRequest(url: String){
-        val fileName = url.substringAfterLast("/")
+    private fun createRequest(urlString: String): Request {
+
+        val fileName = urlString.substringAfterLast("/")
         val directory = ReUseMethods.getPublicFileStorageDir()
+
+        val pathUri = Uri.parse( directory.toString() + File.separator + fileName )
+
+        val request =  Request(urlString, pathUri)
+
+        request.apply {
+            priority = Priority.HIGH
+            networkType = NetworkType.ALL
+            addHeader("clientKey", "SD78DF93_3947&MVNGHE1WONG")
+        }
+
+        return request
     }
 
     override suspend fun save(fileData: FileData) = withContext(ioDispatcher){
@@ -58,5 +76,40 @@ class DefaultRepository(
 
     override suspend fun start(url: String) {
 
+        /* create a request*/
+        val request = createRequest(url)
+
+        /* start the download*/
+        fetch.enqueue(request)
+
+        /* create [FileData] info for file*/
+        val fileData = FileData(
+            0,
+            request.id,
+            url,
+            url.substringAfterLast("/"),
+            0
+        )
+
+        /* add to DB*/
+        save(fileData)
+    }
+
+    override suspend fun pause(id: Int) {
+
+        /*pause download*/
+        fetch.pause(id)
+    }
+
+    override suspend fun resume(id: Int) {
+        fetch.resume(id)
+    }
+
+    override suspend fun restart(url: String) {
+        /* create a request*/
+        val request = createRequest(url)
+
+        /* start the download*/
+        fetch.enqueue(request)
     }
 }
