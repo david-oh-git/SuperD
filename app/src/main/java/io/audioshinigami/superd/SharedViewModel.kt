8b31902a2d24cb719fe2c84d6512c1d8bdbf566b
dave-oh-git <hirakoshinji722@gmail.com>
2019-10-30 12:50:53 +0100

    +++ implement startdownload : SharedViewModel

--- app/src/main/java/io/audioshinigami/superd/SharedViewModel.kt
+++ app/src/main/java/io/audioshinigami/superd/SharedViewModel.kt
@@ -3,10 +3,13 @@ package io.audioshinigami.superd
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tonyodev.fetch2.Fetch
 import io.audioshinigami.superd.data.Result
 import io.audioshinigami.superd.data.repository.DefaultRepository
 import io.audioshinigami.superd.data.source.db.entity.FileData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SharedViewModel( private val repository: DefaultRepository,
                        private val fetch: Fetch ) :
@@ -16,7 +19,20 @@ class SharedViewModel( private val repository: DefaultRepository,
     private val _downloads = MutableLiveData<Result<List<FileData>>>()
     val downloads: LiveData<Result<List<FileData>>> = _downloads
 
-    fun startDownload( url: String ){
+    /* list of currently active downloads*/
+    private var activeDownloads = MutableLiveData<MutableList<String>>()
+
+    val cat = mutableListOf<String>()
+
+    fun startDownload( url: String ) = viewModelScope.launch(Dispatchers.IO) {
+
+        /* start download */
+        repository.start(url)
+
+        /* add to active downloads*/
+        activeDownloads.value?.add(url)
+
+        // TODO attach fetch listener
 
     }
 
