fe35e7d2c0026a8a46869efcf1c4bd3e092acc85
shinji <hirakoshinji722@gmail.com>
2019-12-26 15:52:17 +0100

    remove options menu : SettingsFragment

---
+++ app/src/main/java/io/audioshinigami/superd/settings/SettingsFragment.kt
@@ -0,0 +1,37 @@
+package io.audioshinigami.superd.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.audioshinigami.superd.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+//        remove optionsMenu
+        menu.clear()
+    }
+
+}
