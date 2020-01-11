9ec43ce97d80c522f8b1f02e9a451e3babf5c588
shinji <hirakoshinji722@gmail.com>
2020-01-11 11:28:26 +0100

    +++ createDatabase & createFileSourceInfo: ServiceLocator

--- app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
+++ app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
@@ -2,11 +2,16 @@ package io.audioshinigami.superd
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.room.Room
+import io.audioshinigami.superd.zdata.source.FileInfoSource
+import io.audioshinigami.superd.zdata.source.local.FileDatabase
+import io.audioshinigami.superd.zdata.source.local.LocalFileInfoSource
 
 object ServiceLocator {
 
     @Volatile // could be used from multiple threads
     var sharedPreferences: SharedPreferences? = null
+    private var database: FileDatabase? = null
 
     private val lock = Any()
 
@@ -16,8 +21,23 @@ object ServiceLocator {
             return sharedPreferences ?: createSharedPreference( name, context )
         }
     }
+    
+    private fun createFileInfoSource( context: Context ): FileInfoSource {
+        val database = database ?: createDatabase(context)
+
+        return LocalFileInfoSource(database.fileDataDao())
+    }
 
     private fun createSharedPreference( name: String, context: Context ): SharedPreferences {
         return context.getSharedPreferences( name , 0 )
+    }
+
+    private fun createDatabase( context: Context ): FileDatabase {
+        val result = Room.databaseBuilder(
+            context.applicationContext,
+            FileDatabase::class.java, "filedownload_db"
+        ).build()
+        database = result
+        return result
     }
 }
--- app/src/main/java/io/audioshinigami/superd/zdata/source/local/FileDatabase.kt
+++ app/src/main/java/io/audioshinigami/superd/zdata/source/local/FileDatabase.kt
@@ -1,16 +1,16 @@
 package io.audioshinigami.superd.zdata.source.local
 
 
+
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import io.audioshinigami.superd.data.source.db.dao.FileDataDao
-import io.audioshinigami.superd.data.source.db.entity.FileData
+import io.audioshinigami.superd.zdata.FileInfo
 
 /* Room DB that contains each [FileData]*/
 
-@Database(entities = [FileData::class], version = 1 )
+@Database(entities = [FileInfo::class], version = 1 )
 abstract class FileDatabase : RoomDatabase(){
 
-    abstract fun fileDataDao(): FileDataDao
+    abstract fun fileDataDao(): FileInfoDao
 
 } //end Filedatabase
