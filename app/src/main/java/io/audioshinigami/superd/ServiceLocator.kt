1c72807ae0dfc5ef651055ac7f36888d133ebb37
shinji <hirakoshinji722@gmail.com>
2020-01-12 13:49:53 +0100

    +++ createFetch & numberOfDownloads: ServiceLocator

--- app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
+++ app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
@@ -3,6 +3,10 @@ package io.audioshinigami.superd
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.room.Room
+import com.tonyodev.fetch2.Fetch
+import com.tonyodev.fetch2.FetchConfiguration
+import io.audioshinigami.superd.common.NUMBER_OF_DOWNLOADS_KEY
+import io.audioshinigami.superd.common.SETTINGS_PREF_NAME
 import io.audioshinigami.superd.zdata.source.FileInfoSource
 import io.audioshinigami.superd.zdata.source.local.FileDatabase
 import io.audioshinigami.superd.zdata.source.local.LocalFileInfoSource
@@ -12,16 +16,17 @@ object ServiceLocator {
     @Volatile // could be used from multiple threads
     var sharedPreferences: SharedPreferences? = null
     private var database: FileDatabase? = null
+    private var fetch: Fetch? = null
 
     private val lock = Any()
 
-    fun provideSharedPreference( name: String,  context: Context ): SharedPreferences {
+    internal fun provideSharedPreference( name: String,  context: Context ): SharedPreferences {
 
         synchronized(this){
             return sharedPreferences ?: createSharedPreference( name, context )
         }
     }
-    
+
     private fun createFileInfoSource( context: Context ): FileInfoSource {
         val database = database ?: createDatabase(context)
 
@@ -40,4 +45,21 @@ object ServiceLocator {
         database = result
         return result
     }
+
+    /* creates fetch instance*/
+    private fun createFetch(numberOfDownloads: Int = 3 , context: Context ): Fetch {
+        val fetchConfig = FetchConfiguration.Builder(context.applicationContext )
+            .setDownloadConcurrentLimit(numberOfDownloads)
+            .build()
+
+        return Fetch.Impl.getInstance(fetchConfig)
+    }
+
+    private fun provideNumberOfDownloads( context: Context): Int {
+        val numberOfDownloads = provideSharedPreference( SETTINGS_PREF_NAME , context)
+            .getInt( NUMBER_OF_DOWNLOADS_KEY , -99)
+
+        return if( numberOfDownloads == -99) 3 else numberOfDownloads
+    }
+
 }
--- app/src/main/java/io/audioshinigami/superd/common/Constants.kt
+++ app/src/main/java/io/audioshinigami/superd/common/Constants.kt
@@ -8,3 +8,7 @@ const val SD_REQCODE = 567
 const val TEST_URL = "http://ccfmoline.com/audiofiles/09_27_09_Tonya_Campbell_Testimony.MP3"
 
 const val TABLE_NAME = "download_table"
+
+const val NUMBER_OF_DOWNLOADS_KEY = "number_of_downloads"
+
+const val SETTINGS_PREF_NAME = "settings"
