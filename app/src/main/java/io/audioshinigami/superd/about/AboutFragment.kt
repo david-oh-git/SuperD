31df7351799de383ca0b56b8da58b69a1ad5c865
shinji <hirakoshinji722@gmail.com>
2020-03-02 19:11:50 +0100

    add about fragments logic

--- app/src/main/java/io/audioshinigami/superd/about/AboutFragment.kt
+++ app/src/main/java/io/audioshinigami/superd/about/AboutFragment.kt
@@ -1,5 +1,7 @@
 package io.audioshinigami.superd.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -7,8 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.audioshinigami.superd.R
+import kotlinx.android.synthetic.main.about_fragment.*
 
 class AboutFragment : Fragment() {
+
+    private val developerGithubUrl = "https://www.github.com/david-oh-git"
+    private val freepikUrl = "https://www.flaticon.com/authors/freepik"
+    private val catalinUrl = "https://www.flaticon.com/authors/catalin-fertu"
+    private val alfredoUrl = "https://www.flaticon.com/authors/alfredo-hernandez"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,4 +35,35 @@ class AboutFragment : Fragment() {
         menu.clear()
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClickListeners()
+    }
+
+    private fun initClickListeners(){
+        developer_item.setOnClickListener {
+            openWebPage(developerGithubUrl)
+        }
+
+        freepik_icon_item.setOnClickListener {
+            openWebPage(freepikUrl)
+        }
+
+        download_icon_item.setOnClickListener {
+            openWebPage(catalinUrl)
+        }
+
+        alfredo_icon_item.setOnClickListener {
+            openWebPage(alfredoUrl)
+        }
+    }
+
+    private fun openWebPage( url: String){
+        val webPage = Uri.parse(url)
+        val intent =  Intent( Intent.ACTION_VIEW, webPage )
+        startActivity(intent)
+    }
+
 }
+
