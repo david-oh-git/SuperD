0d22adc6389bd62ebb310891ad93b118163c82c4
david <hirakoshinji722@gmail.com>
2020-05-08 16:09:25 +0100

    remove unused code : ServiceLocator object

--- app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
+++ app/src/main/java/io/audioshinigami/superd/ServiceLocator.kt
@@ -29,46 +29,9 @@ object ServiceLocator {
     @Volatile
     private var activeDownloads: MutableMap<Int, State>? = null
 
-    @Volatile
-    var fetch: Fetch? = null
-
-    private val lock = Any()
-
-    internal fun provideFileInfoRepository( context: Context): FileInfoRepository {
-        synchronized(this){
-            return fileInfoRepository ?: createFileInfoRepository( context )
-        }
-    }
-
     internal fun provideSharedPreferenceRepository( name: String, context: Context): SharedPreferenceRepo {
         synchronized(this){
             return preferenceRepository ?: createPreferenceRepository(name, context)
-        }
-    }
-
-    internal fun provideFetch( context: Context ): Fetch {
-        synchronized(this){
-            val defaultFetch = fetch
-            if( defaultFetch == null || defaultFetch.isClosed ){
-                val newFetch = createFetch(context , getNumberOfDownloads(context))
-                fetch = newFetch
-                return newFetch
-            }
-            return fetch ?: createFetch(context, getNumberOfDownloads(context))
-        }
-    }
-
-    private fun getNumberOfDownloads(context: Context): Int {
-        val numberOfDownloads = provideSharedPreference( SETTINGS_PREF_NAME , context.applicationContext)
-            .getInt( NUMBER_OF_DOWNLOADS_KEY , -99)
-
-        return if( numberOfDownloads == -99) 2 else numberOfDownloads
-    }
-
-    internal fun provideSharedPreference( name: String,  context: Context ): SharedPreferences {
-
-        synchronized(this){
-            return sharedPreferences ?: createSharedPreference( name, context )
         }
     }
 
@@ -78,33 +41,9 @@ object ServiceLocator {
         }
     }
 
-    private fun createFileInfoRepository( context: Context ): FileInfoRepository {
-        val newRepo = DefaultRepository( createFileInfoSource(context),
-            createDownloadDataSource(context))
-
-        fileInfoRepository = newRepo
-        return newRepo
-    }
-
     private fun createPreferenceRepository( name: String ,context: Context): SharedPreferenceRepo {
         return DefaultPreferenceRepository(
             createLocalPreferenceSource(name, context )
-        )
-    }
-
-    private fun createFileInfoSource( context: Context ): FileInfoSource {
-        val database = database ?: createDatabase(context)
-
-        return LocalFileInfoSource(database.fileDataDao())
-    }
-
-    private fun createDownloadDataSource( context: Context ): DownloadDataSource {
-        val database = database ?: createDatabase(context)
-
-        return RemoteDownloadDataSource(
-            provideFetch( context.applicationContext )
-            , database.fileDataDao(),
-            ( (context.applicationContext as App) )
         )
     }
 
@@ -116,27 +55,6 @@ object ServiceLocator {
         return LocalPreferenceSource(
             createSharedPreference(name, context)
         )
-    }
-
-    private fun createDatabase( context: Context ): FileDatabase {
-        val result = Room.databaseBuilder(
-            context.applicationContext,
-            FileDatabase::class.java, "filedownload_db"
-        ).build()
-        database = result
-        return result
-    }
-
-    /* creates fetch instance*/
-    private fun createFetch( context: Context ,numberOfDownloads: Int ): Fetch {
-        val fetchConfig = FetchConfiguration.Builder(context.applicationContext )
-            .setDownloadConcurrentLimit(numberOfDownloads)
-            .enableLogging(true)
-            .build()
-
-        val newFetch = Fetch.Impl.getInstance(fetchConfig)
-        fetch = newFetch
-        return newFetch
     }
 
     @VisibleForTesting
