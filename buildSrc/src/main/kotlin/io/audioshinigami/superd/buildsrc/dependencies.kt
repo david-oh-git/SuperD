c39311761a3f9e36629c64d351924c3d03b56c2e
dave oh <hirakoshinji722@gmail.com>
2021-02-25 09:52:58 +0100

    update kotlin and add kotlin DSL

---
+++ buildSrc/src/main/kotlin/io/audioshinigami/superd/buildsrc/dependencies.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.audioshinigami.superd.buildsrc
+
+object Versions {
+    const val ktlint = "0.40.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha08"
+    const val jdkDesugar = "com.android.tools:desugar_jdk_libs:1.0.9"
+
+    const val material = "com.google.android.material:material:1.1.0"
+
+    object Accompanist {
+        private const val version = "0.6.0"
+        const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
+        const val insets = "dev.chrisbanes.accompanist:accompanist-insets:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.4.30"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.4.2"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object OkHttp {
+        private const val version = "4.9.0"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val logging = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.2.0"
+        const val palette = "androidx.palette:palette:1.0.0"
+
+        const val coreKtx = "androidx.core:core-ktx:1.5.0-beta01"
+
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha03"
+        }
+
+        object Constraint {
+            const val constraintLayoutCompose = "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha03"
+        }
+
+        object Compose {
+            private const val snapshot = ""
+            private const val version = "1.0.0-beta01"
+
+            @get:JvmStatic
+            val snapshotUrl: String
+                get() = "https://androidx.dev/snapshots/builds/$snapshot/artifacts/repository/"
+
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.3.0-beta01"
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha02"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2-rc01"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+
+        object Room {
+            private const val version = "2.2.5"
+            const val runtime = "androidx.room:room-runtime:$version"
+            const val ktx = "androidx.room:room-ktx:$version"
+            const val compiler = "androidx.room:room-compiler:$version"
+        }
+    }
+
+    object Rome {
+        private const val version = "1.14.1"
+        const val rome = "com.rometools:rome:$version"
+        const val modules = "com.rometools:rome-modules:$version"
+    }
+}
